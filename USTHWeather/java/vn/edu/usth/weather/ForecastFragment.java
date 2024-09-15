8161149384356d01282fac3ffd8aab9d8348a2c8
catcandance2004 <catcandance2004@gmail.com>
2024-09-15 17:58:44 +0700

    prac 4

--- USTHWeather/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/java/vn/edu/usth/weather/ForecastFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,9 +61,22 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(0x20FF0000);
-        return view;
+        LinearLayout linearLayout = new LinearLayout(getContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setBackgroundColor(0x2000FF00);
+        TextView thursdayTextView = new TextView(getContext());
+        thursdayTextView.setText(R.string.day_thursday);
+        thursdayTextView.setTextSize(20);
+        ImageView thursdayImageView = new ImageView(getContext());
+        thursdayImageView.setImageResource(R.drawable.weather);
+        thursdayImageView.setLayoutParams(new LinearLayout.LayoutParams(
+                160, 160));
+        linearLayout.addView(thursdayTextView);
+        linearLayout.addView(thursdayImageView);
+
+        return linearLayout;
+        /*
+        ImageView:
+         */
     }
 }
